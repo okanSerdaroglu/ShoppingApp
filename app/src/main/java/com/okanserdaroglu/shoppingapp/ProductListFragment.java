3c718640f9fa3e56b169ef254001688e4c2f6073
Okan Serdaroğlu <okanserdaroglu2@gmail.com>
2019-09-21 11:17:15 +0300

    navigation graph added

---
+++ app/src/main/java/com/okanserdaroglu/shoppingapp/ProductListFragment.java
@@ -0,0 +1,23 @@
+package com.okanserdaroglu.shoppingapp;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class ProductListFragment extends Fragment {
+
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.fragment_product_list,container,false);
+
+    }
+
+}
